23b51a6d92dd95c46124bc3dd60a3d0b9604a2d8
juan-ivan-NV <coldcram14@hotmail.com>
2020-08-13 11:53:42 -0500

    sec 32 testing static data

--- sections/EndToEndAutomation/TestCases/test_TC_001_Registration_ValidData.py
+++ sections/EndToEndAutomation/TestCases/test_TC_001_Registration_ValidData.py
@@ -2,10 +2,17 @@ from selenium.webdriver import Chrome
 from Base import InitiateDriver
 from Library import ConfigReader
 from Pages import registrationPage
+import pytest
 
-def test_ValidateRegistration():
+def dataGenerator():
+    li = [['uname1','pass1'],['uname2','pass2'],['uname3','pass3']]
+    return li
+
+@pytest.mark.parametrize('data',dataGenerator())
+def test_ValidateRegistration(data):
     driver = InitiateDriver.startBrowser()
     register = registrationPage.RegistrationClass(driver)
-    register.enter_username('hello')
-    register.enter_password('abcd')
+    register.enter_username(data[0])
+    register.enter_password(data[1])
     #register.enter_email('hello')
+
