de87e0a330d82e05dc8cb489b57a97403b41d95b
juan-ivan-NV <coldcram14@hotmail.com>
2020-09-04 16:56:23 -0500

    api advanced path

--- sections/APIAutomation/GET_Request/FetchUserData.py
+++ sections/APIAutomation/GET_Request/FetchUserData.py
@@ -1,22 +1,20 @@
 import requests
+import json
+import jsonpath
 
 # API URL
 url = "https://reqres.in/api/users?page=2"
 
 # Send Get Request
 response = requests.get(url)
-print(response)
 
-# Validate Status Code
-assert response.status_code == 200
-# Fetch Response Header
-print(response.headers)
-print(response.headers.get('Date'))
-print(response.headers.get('Server'))
+# Parse response to Json format
 
-# Fetch Cookies
-print(response.cookies)
-# Fetch Encoding
-print(response.encoding)
+json_response = json.loads(response.text)
+#print(json_response)
 
-print(response.elapsed)
+# Fetch value using Json Path
+
+for i in range(0,3):
+    first_name = jsonpath.jsonpath(json_response, 'data['+str(i)+'].first_name')
+    print((first_name[0]))
